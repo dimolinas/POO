6a9ae90d4ed2656b5d45c92f7cd0e48dbb343258
Diego Molina <dimolinas@unal.edu.co>
2020-03-21 13:40:50 -0500

    Hola gato

--- Tarea2/src/com/company/Main.java
+++ Tarea2/src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args){
         System.out.println("HELLO WORLD");
         System.out.println("Pruebas de commit");
+        System.out.println("Hola me llama Minino");
     }
 }

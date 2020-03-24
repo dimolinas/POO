8b27530be11941ceb022fd4a7e34991ffeb3283b
Diego Molina <dimolinas@unal.edu.co>
2020-03-24 16:16:30 -0500

    Imprime letras ingresadas

--- Tarea2/src/com/company/Main.java
+++ Tarea2/src/com/company/Main.java
@@ -2,6 +2,9 @@ package com.company;
 
 
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -10,9 +13,10 @@ public class Main {
         return (int) Math.floor(Math.random() * 10 + 0);  // Retorna numero aleatorio
     }
 
-    public static String leerLetras() {
+    public static String leerLetras(List lista) {
         Scanner lector = new Scanner(System.in); //Lee la letra ingresada por el usuario
-        return (String) lector.nextLine();
+        String letra = lector.nextLine();
+        return letra;
     }
 
     public static void imprimirPalabra(String arreglo[]) {
@@ -22,13 +26,24 @@ public class Main {
     }
 
 
-    public static void imprimirSignoPerdida(String arreglo[], int parametro) {
-        if (parametro != 0) {
+    public static void imprimirDatos(String arreglo[], int parametro,List lista) {
+        System.out.print("          "); // Imprime espacio de sobra
+
+        Iterator iterador = lista.iterator(); //Recorrer Una lista método muy rebuscado
+        while (iterador.hasNext()){
+            System.out.print(iterador.next()+" ");
+        }
+
+        System.out.print("          "); // Imprime espacio de sobra
+
+        if (parametro != 0) { //Imprime signo perdida
             for (int i = 0; i < parametro; i++) {
                 System.out.print(arreglo[i]);
             }
         }
+        System.out.println();
     }
+
     public static void espaciosVacios(String arreglo[]) {
         for (int i = 0; i < arreglo.length; i++) {
             arreglo[i] = "_";
@@ -46,7 +61,8 @@ public class Main {
         return control;
     }
 
-    public static boolean finalizar(String pal[], String palabra) { //No encontré la manera de darle finalización a esto :( | Me siento re miserable
+    public static boolean finalizar(String pal[], String palabra) {
+        //No encontré la manera de darle finalización a esto :( | Me siento re miserable
         String palabraEnLista = String.join("", pal);
         boolean terminado = false;
         if (palabraEnLista.equals(palabra)) {
@@ -78,26 +94,33 @@ public class Main {
         signoPerdida[5] = ")";
         signoPerdida[6] = "p";
 
+        List letrasIngresadas = new ArrayList(); //Lista para las palabras ingresadas
+
         String palabraMagica = palabrasMagicas[numeroAleatorio()]; //Palabra escogida aleatoriamente, se escoge una palabra cualquiera según el número aleatorio aquél
+
         String[] palabraVacia = new String[palabraMagica.length()]; //Arreglo palabra vacia
         espaciosVacios(palabraVacia); //Se ponen los espacios de las letras de la palabra
+
         int vidas = 7; //Numero de intentos
 
 
         while (vidas > 0) {
+
             System.out.println("--------------------------------------------------");
             System.out.println("                     Hangman                      ");
             System.out.println("--------------------------------------------------");
             System.out.println("Te quedan " + vidas + " vidas");
+
             imprimirPalabra(palabraVacia);
-            System.out.print("          "); // Imprime espacio de sobra
-            imprimirSignoPerdida(signoPerdida, (7 - vidas));
-            System.out.println();
-            //String ingreso = (leerLetras());
-            //System.out.print("    " + ingreso);
-            if (!buscadorLetras(palabraMagica, palabraVacia, leerLetras())) {
+            imprimirDatos(signoPerdida, (7 - vidas), letrasIngresadas);
+
+            String letraEntrada = leerLetras(letrasIngresadas);
+            letrasIngresadas.add(letraEntrada);
+
+            if (!buscadorLetras(palabraMagica, palabraVacia, letraEntrada)) {
                 vidas -= 1;
             }
+
             if (finalizar(palabraVacia, palabraMagica)) {
                 imprimirPalabra(palabraVacia);
                 System.out.println("You Win!!!");
@@ -106,9 +129,7 @@ public class Main {
 
         }
         if (vidas == 0) {
-            System.out.print("You Lose!!!The secret word is: " + palabraMagica); //Imprime la palabra al final
+            System.out.print("You Lose!!!The secret word is: " + palabraMagica); //Mesanje perdiste
         }
-
-
     }
 }
